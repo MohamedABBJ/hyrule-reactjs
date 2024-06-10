80d761daef8d71c51af96fba7b1f81aecd79da5f
unknown <mohamedabbj@gmail.com>
2024-06-10 13:51:14 -0700

    improving design

--- Frontend/src/components/navbar/navbar.jsx
+++ Frontend/src/components/navbar/navbar.jsx
@@ -2,30 +2,34 @@ import { AccountCircle, Image } from "@mui/icons-material";
 import { Box, Button, Icon, IconButton, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
+import EnemiesBtn from "../anims/enemiesBtn";
 
 const NavBar = (type) => {
   const conditionTypeTop = type.props == "top";
   const conditionTypeSide = type.props == "side";
 
   const navbarForm = {
-    height: conditionTypeTop ? "8vh" : conditionTypeSide ? "100vh" : "",
+    height: conditionTypeTop ? "50px" : conditionTypeSide ? "100vh" : "",
     width: conditionTypeTop ? "100%" : conditionTypeSide ? "6%" : "",
     position: conditionTypeTop ? "relative" : conditionTypeSide ? "fixed" : "",
   };
 
   return (
     <>
+    <Box  sx={{backgroundColor:'#2c387e'}} height={'15px'}></Box>
       <Box
         display={"flex"}
         alignItems={"center"}
         height={navbarForm.height}
         width={navbarForm.width}
-        bgcolor={"aqua"}
+        bgcolor={'#3f51b5'}
         position={navbarForm.position}
       >
-        <Box display={'flex'} alignItems={'center'} height={'100%'} border={"solid"} width={"100%"}>
+        <Box sx={{'& button': {color:'white', fontFamily:'HyruleFont', fontSize:'1.3rem'}, '& a':{marginLeft:'50px'}}} display={'flex'} alignItems={'center'} height={'100%'} width={"100%"}>
           <Link to={"/"}>
-            <Box component={"img"} src="../../public/assets/" />
+            <Button>
+              Home
+            </Button>
           </Link>
           <Link to={"/items"}>
             <Button>
@@ -34,8 +38,8 @@ const NavBar = (type) => {
           </Link>
           <Link>
           <Button>
-                Enemies
-            </Button>
+            <EnemiesBtn/>     
+          </Button>
           </Link>
           <Link>
           <Button>
@@ -48,14 +52,14 @@ const NavBar = (type) => {
             </Button>
         </Link>
         </Box>
-        <Box position={'relative'} display={'flex'} alignItems={'center'} justifyContent={'end'} height={'100%'} width={"20%"} border={"solid"}>
+        <Box position={'relative'} display={'flex'} alignItems={'center'} justifyContent={'end'} height={'100%'} width={"20%"}>
           <Link style={{height:'100%', display:'flex', alignItems:'center', position:'relative'}}>
           <Button sx={{height:'100%'}}>
            <Box height={'100%'} component={'img'} src="../../../public/assets/github-logo.webp"/>
           </Button>
           </Link>
           <Link style={{height:'100%', display:'flex', alignItems:'center', position:'relative'}}>          
-            <IconButton sx={{height:'100%'}} children={<AccountCircle sx={{width:'130%', height:'130%'}}/>}/>
+            <IconButton sx={{height:'100%'}} children={<AccountCircle sx={{width:'130%', height:'130%', color:'white'}}/>}/>
           </Link>
         </Box>
       </Box>
