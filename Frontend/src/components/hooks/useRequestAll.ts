10530823288100d11e560499766bd33c7e7b1f35
unknown <mohamedabbj@gmail.com>
2024-06-21 10:23:59 -0700

    adding types to useRequestAll

--- Frontend/src/components/hooks/useRequestAll.ts
+++ Frontend/src/components/hooks/useRequestAll.ts
@@ -2,15 +2,19 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
+interface DataValues{
+  dataValue:{}
+}
+
 const useRequestAll = () =>{
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(<DataValues>({dataValue:{}}));
+  const [loading, setLoading] = useState(<boolean>(false));
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const {data: dataval} = await axios.get('https://botw-compendium.herokuapp.com/api/v3/compendium/all');
-        setData(dataval);
+        const {data: dataVal} = await axios.get('https://botw-compendium.herokuapp.com/api/v3/compendium/all');
+        setData({dataValue:dataVal});
       } catch (error) {
         console.error(error)
       }
